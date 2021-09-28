a64dd7fceb0e9f2de7a83af975b2e3234a172b1e
domsq <Domsq@hotmail.com>
2021-09-28 13:06:04 +0000

    Initial commit

--- map.js
+++ map.js
@@ -5,16 +5,36 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
 
+console.log(results);
 
 // Using map()
 
+const multByTwo = function(num) {
+  return num * 2;
+}
+
+// function multByTwo(num) {
+//   return num * 2;
+// }
+
+// const multByTwo = (num) => num * 2;
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const simple = nums.map(function(num) { return num * 2});
+console.log(simple);
 
 // Simplfied w/ map() + arrow function
-
+const simple2 = nums.map(num => num * 2);
+console.log(simple2)
 
 // With objects:
 const students = [
@@ -37,3 +57,5 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map()
